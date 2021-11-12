47985b84561f373b75894a4f77fdf92fc9fbc6a6
hiromu624 <hiromu.music.624@gmail.com>
2021-11-12 10:32:07 +0000

    2nd commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b9bad5709ed8fe9128b2b4d9ddec415f0623f9111cfa2e48552e39261a5f710b5aad49122b6f55a30e59ba873f8374d45c30d34061ce32caa52b5b5c0c40086c'
